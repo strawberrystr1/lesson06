b700d555c1f954f6b40b0feff2406c1442c5d682
Andrei Shakh <andreishakhs@gmail.com>
2020-04-02 12:15:59 +0300

    hard

--- script.js
+++ script.js
@@ -5,32 +5,53 @@ const isNumber = function (n) {
 };
 
 const gameBoy = function () {
-
+    let set;
+    let trying = 3;
     let number = Math.floor(Math.random() * 102); //загаданное число
     console.log('number: ', number);
+
+    function count () {
+        trying--;
+        if (trying === 0) {
+           set = confirm('Попытки закончились, хотите сыграть еще?');
+        }
+        if (set) {
+            gameBoy();
+        } else {
+            return;
+        }
+        console.log(set);
+        return set;
+    }   
+   
     function attempt () {
         let ask = prompt('Угадай число от 1 до 100'); // число пользователя
-
+  
         if (ask === null) {
-            return;
+            return alert(`Игра закончена!`);
         }
 
         if(!isNumber(ask)) {
             confirm('Введи число!');
             attempt();
-        }
-        
-        if (ask === number) {
-            alert ('Вы угадали число!');
-        } else if (ask > number) {
-            if (confirm('Загаданное число меньше!')) {
+        } 
+
+        if (ask > number) {
+            count();
+            if (confirm(`Загаданное число меньше, осталось попыток ${trying}!`) && set !== false) {
             attempt();
-            } else {return;}
+            } else {return alert(`Игра закончена!`);}
         } else if (ask < number) {
-            if (confirm('Загаданное число больше')) {
+            count();
+            if (confirm(`Загаданное число больше, осталось попыток ${trying}`) && set !== false) {
             attempt();
-            } else {return;}
+            } else {return alert(`Игра закончена!`);}
+        } else if (+ask === number) {
+           if (confirm('Вы угадали число! Хотели бы сыграть еще?')) {
+               gameBoy();
+           } else {return alert(`Игра закончена!`);}
         }
+ 
     }
     
    attempt();
